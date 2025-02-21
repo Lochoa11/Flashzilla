bcbb3a1a914ca92b77f02ab8986066f47f6e285a
Lochoa11 <linochoa11690@gmail.com>
2025-02-21 05:04:26 -0500

    Initial Commit

---
+++ FlashzillaTests/FlashzillaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaTests.swift
+//  FlashzillaTests
+//
+//  Created by Lin Ochoa on 2/21/25.
+//
+
+import Testing
+@testable import Flashzilla
+
+struct FlashzillaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
